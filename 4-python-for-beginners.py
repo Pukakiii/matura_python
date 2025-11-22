5e64d88a33520f469d8c8013d61102220cf3806d
Pukaki <pukaki.ip@gmail.com>
2025-11-22 17:35:30 +0100

    4

---
+++ 4-python-for-beginners.py
@@ -0,0 +1,50 @@
+# INPUT PRACTISE / STRING FUNCTIONALITY / ARYTHMETIC OPERATIONS and other functionality
+
+"""
+name=input('name: ')
+color=input('color: ')
+print(name+' and '+color)
+"""
+""" 
+birth_year=input('year: ')
+age=2025-int(birth_year)
+print(age)
+ 
+weight=input('kg: ')
+age=1000*int(weight)
+print(age, 'g')
+"""
+
+# multi_line_string="""Hi
+# my
+#      neightor 
+#  """
+# print(multi_line_string)
+
+""" 
+course='python for beginners'
+print(course[-1])
+print(course[0:3])
+print(course[1:])
+print(course[:5])
+copy=course[:]
+print(copy)
+print(course[1:-1])
+"""
+""" 
+course1='python for beginners'
+formated_string=f'is it {course1}?'
+# formated_string.upper() - not updating
+print(len(formated_string))
+print(formated_string.upper())
+print(formated_string.title())
+print(formated_string.find('o')) # - case sensitive
+print(formated_string.find('beginners'))
+print(formated_string.replace('beginners', 'pros'))
+print('pros' in formated_string)
+"""
+print(5//2)
+print(5%2)
+print(abs(-2.4))
+print(round(-2.4))
+print(round(-2.6))
